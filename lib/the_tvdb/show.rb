8853a6f674ccb7efa3f6f244302cd504413baaeb
Alessandro Mencarini <a.mencarini@freegoweb.it>
2012-12-19 23:26:40 +0100

    Characterization tests to be complted

--- lib/the_tvdb/show.rb
+++ lib/the_tvdb/show.rb
@@ -1,8 +1,23 @@
 module TheTvdb
   class Show
     
+    attr_accessor :id, :name, :banner, :description, :first_aired
+    
     def self.search(name)
-      TheTvdb.gateway.get_series(name)
+      shows = TheTvdb.gateway.get_series(name)
+      shows.map {|s| Show.new(s) }
+    end
+
+    def initialize(info)
+      @id = info['seriesid'].to_i
+      @name = info['SeriesName']
+      @banner = info['banner']
+      @description = info['Overview']
+      @first_aired = info['FirstAired']
+    end
+        
+    def episodes
+      []
     end
 
   end
--- spec/lib/the_tvdb/show_spec.rb
+++ spec/lib/the_tvdb/show_spec.rb
@@ -2,22 +2,33 @@ require_relative '../../spec_helper'
 # For Ruby < 1.9.3, use this instead of require_relative
 # require (File.expand_path('./../../../spec_helper', __FILE__))
 describe TheTvdb::Show do
-  
-  describe "search" do
-    before do
-      VCR.insert_cassette 'series_search', record: :new_episodes
-    end
-      
-    after do
-      VCR.eject_cassette
-    end
+
+  before do
+    VCR.insert_cassette 'series', record: :new_episodes
+  end
     
+  after do
+    VCR.eject_cassette
+  end
+  
+  let(:shows) { TheTvdb::Show.search('Game of Thrones') }
+  let(:show) { shows.first }
+  
+  describe "#search" do
     it "should return related series when searching for a show" do
-      shows = TheTvdb::Show.search('Game of Thrones')
       shows.size.should be(1)
-      show = shows.first
-      show['SeriesName'].should == 'Game of Thrones'
+      show.name.should == 'Game of Thrones'
+      show.id.should == 121361
+      description = 'Based on the fantasy novel series "A Song of Ice and Fire," Game of Thrones explores the story of an epic battle among seven kingdoms and two ruling families in the only game that matters - the Game of Thrones. All seek control of the Iron Throne, the possession of which ensures survival through the 40-year winter to come.'
+      show.description.should == description
     end
   end
+
+  describe "#find" do
+    it "should return episodes related series when searching for a show" do
+      show.should have(3).episodes
+    end
+  end
+
 
 end
